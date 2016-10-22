df9da9196081b69985f2f550e17fa6149e55f59c
Neeraj <neeraj@DoorMints-MacBook-Pro.local>
2016-10-22 17:44:57 +0530

    adding default action mailer to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :info
   config.logger = Logger.new("/var/log/hotel/production.log")
-
+  config.action_mailer.default_url_options = { host: 'hotelashokachomu.com', port: 3000 }
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
