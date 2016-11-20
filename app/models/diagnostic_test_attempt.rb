6c47cb968339e109fcb4514c3d87aa0a5ae79aa1
Neeraj <neeraj@DoorMints-MacBook-Pro.local>
2016-11-21 01:51:15 +0530

    correction

--- app/models/diagnostic_test_attempt.rb
+++ app/models/diagnostic_test_attempt.rb
@@ -6,8 +6,9 @@ class DiagnosticTestAttempt < ActiveRecord::Base
     stream_hash = {}
     ShortChoiceQuestion.where(:id => question_answers.keys).includes(:stream, :second_topic).each do |q|
       stream_hash[q.stream_id] ||= {'other_details' => {'stream_name' => q.stream.name}, 'second_topics' => {}}
-      stream_hash[q.stream_id]['second_topics'][q.second_topic_id] ||= {'second_topic_name' => q.second_topic.name}
-      stream_hash[q.stream_id]['second_topics'][q.second_topic_id] = {'score' => question_answers[q.id.to_s]['score']}
+      stream_hash[q.stream_id]['second_topics'][q.second_topic_id] ||= {}
+      stream_hash[q.stream_id]['second_topics'][q.second_topic_id] = {'score' => question_answers[q.id.to_s]['score'],
+      'second_topic_name' => q.second_topic.name}
       
       if question_answers[q.id.to_s]['score'] == 0
         new_lowest_position = q.second_topic.stream_position
